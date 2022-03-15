c5ee6e728827efe23b60a12c47be6d1515616274
komkawila <kompanuwat@gmail.com>
2022-03-15 16:35:45 +0700

    1

--- sttslife-api/util/db.config.js
+++ sttslife-api/util/db.config.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 const db = mysql.createConnection({
     host: "localhost",
-    user: "komadmin",
+    user: "admin",
     password: "P@ssw0rd",
     database: "saverair_db",
 });
